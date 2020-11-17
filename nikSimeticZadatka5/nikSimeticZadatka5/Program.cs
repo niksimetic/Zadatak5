320e9eab97a70ad777164717fb90f2d9a21ae481
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-11-17 10:58:43 +0100

    Add files via upload

---
+++ nikSimeticZadatka5/nikSimeticZadatka5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikSimeticZadatka5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zbroj = 0;
+            int karta;
+            Console.WriteLine("Unesite brojeve karta (1-13): ");
+
+            do
+            {
+                karta = Convert.ToInt32(Console.ReadLine());
+                if (karta > 0 && karta < 14)
+                {
+                    zbroj = zbroj + karta;
+
+                }
+                else
+                {
+                    Console.WriteLine("x");
+                }
+            } while (zbroj < 31);
+            if(zbroj == 31)
+            {
+                Console.WriteLine("Pobijedili ste ");
+
+            }
+            else
+            {
+                Console.WriteLine("Izgubili ste");
+                Console.ReadKey();
+            }
+        }
+    }
+}
